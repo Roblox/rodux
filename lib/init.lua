2a705369835a897afa6caa43497dfd02fa3ae876
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-03-05 13:09:18 -0500

    add prototype logger middleware

--- lib/init.lua
+++ lib/init.lua
@@ -1,9 +1,11 @@
 local Store = require(script.Store)
 local createReducer = require(script.createReducer)
 local combineReducers = require(script.combineReducers)
+local loggerMiddleware = require(script.loggerMiddleware)
 
 return {
 	Store = Store,
 	createReducer = createReducer,
 	combineReducers = combineReducers,
+	logger = loggerMiddleware,
 }
---
+++ lib/loggerMiddleware.lua
@@ -0,0 +1,41 @@
+local indentStr = "    "
+
+local function prettyPrint(t, indent)
+	indent = indent or 1
+	local outputBuffer = {
+		"{\n"
+	}
+
+	for key, value in pairs(t) do
+		local strKey = tostring(key)
+
+		table.insert(outputBuffer, indentStr:rep(indent + 1))
+		table.insert(outputBuffer, strKey)
+		table.insert(outputBuffer, " = ")
+
+		if type(value) == "table" then
+			table.insert(outputBuffer, prettyPrint(value, indent + 1))
+			table.insert(outputBuffer, "\n")
+		else
+			table.insert(outputBuffer, tostring(value))
+			table.insert(outputBuffer, "; (")
+			table.insert(outputBuffer, typeof(value))
+			table.insert(outputBuffer, ")\n")
+		end
+	end
+
+	table.insert(outputBuffer, indentStr:rep(indent))
+	table.insert(outputBuffer, "}")
+
+	return table.concat(outputBuffer, "")
+end
+
+local function loggerPlugin(next)
+	return function(store, action)
+		print("Action dispatched:", prettyPrint(action))
+		next(action)
+		print("State changed to:", prettyPrint(store:getState()))
+	end
+end
+
+return loggerPlugin
