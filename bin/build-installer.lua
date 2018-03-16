54a8cd472b875af0a959d6159fa5abdc2ca17ae8
Lucien Greathouse <me@lpghatguy.com>
2018-03-16 14:26:17 -0700

    Convert to Git submodules for dependencies, delete bin scripts

--- bin/build-installer.lua
+++
@@ -1,9 +0,0 @@
---[[
-	Generates an install script using rbxpacker.
-
-	It should be run from the project directory, like:
-
-		lua bin/build-installer.lua
-]]
-
-os.exit(os.execute("rbxpacker --exclude **/*.spec.lua --folder Rodux --name Rodux lib LICENSE > installer.lua"))
