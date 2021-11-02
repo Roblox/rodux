e850da073c085f03b368eb37fb78a2778682e206
Matt Hargett <plaztiksyke@gmail.com>
2021-11-02 11:29:52 -0700

    Incorrect parenthesizing

--- src/getFunctionName.lua
+++ src/getFunctionName.lua
@@ -3,8 +3,8 @@ local function getFunctionName(fn)
 	if typeof(fn) == "function" then
 		-- try using Roblox Lua's debug.info before falling back to lua5.1 debug.getinfo
 		-- selene: allow(incorrect_standard_library_use)
-		functionName = (type(debug.info) == "function" and debug.info(fn, "sln"))
-			or (type(debug.getinfo) == "function" and debug.getinfo(fn).source)
+		functionName = (type(debug.info) == "function") and debug.info(fn, "sln")
+			or (type(debug.getinfo) == "function") and debug.getinfo(fn).source
 	end
 	if functionName == nil or functionName == "" then
 		functionName = tostring(fn)
