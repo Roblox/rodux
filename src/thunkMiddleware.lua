63825709a13050a7c043136b79f4d00587124ea1
Tony Cuadra <tcuadra@roblox.com>
2022-03-24 06:45:56 -0700

    Add extra argument to thunk middleware (#69)
    This feature is equivalent to what is available in newer versions of Redux:
    https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
    
    It allows arbitrary parameters to injected into thunks via a table passed in at Rodux initialization time (useful for passing in services or anything else we might want to mock for tests).
    
    It can replace other custom middlewares we've written to achieve the same thing (e.g. PaginatedFetchThunk.middleware, AvatarEditorServiceThunk.middleware, RNAdapterThunk.middleware)

--- src/thunkMiddleware.lua
+++ src/thunkMiddleware.lua
@@ -4,31 +4,6 @@
 	This middleware consumes the function; middleware further down the chain
 	will not receive it.
 ]]
-local function tracebackReporter(message)
-	return debug.traceback(message)
-end
+local makeThunkMiddleware = require(script.Parent.makeThunkMiddleware)
 
-local function thunkMiddleware(nextDispatch, store)
-	return function(action)
-		if typeof(action) == "function" then
-			local ok, result = xpcall(function()
-				return action(store)
-			end, tracebackReporter)
-
-			if not ok then
-				-- report the error and move on so it's non-fatal app
-				store._errorReporter.reportReducerError(store:getState(), action, {
-					message = "Caught error in thunk",
-					thrownValue = result,
-				})
-				return nil
-			end
-
-			return result
-		end
-
-		return nextDispatch(action)
-	end
-end
-
-return thunkMiddleware
+return makeThunkMiddleware(nil) -- no extra argument
