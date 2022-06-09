529fd8235495eeaf79881706a91338f64b032230
jkelaty <jkelaty@roblox.com>
2022-06-09 13:26:09 -0700

    Add thunks/store types

--- src/init.lua
+++ src/init.lua
@@ -8,12 +8,23 @@ local makeThunkMiddleware = require(script.makeThunkMiddleware)
 
 local actions = require(script.types.actions)
 local reducers = require(script.types.reducers)
+local store = require(script.types.store)
+local thunks = require(script.types.thunks)
 
 export type Action<Type = any> = actions.Action<Type>
 export type AnyAction = actions.AnyAction
 export type ActionCreator<Type, Action, Args...> = actions.ActionCreator<Type, Action, Args...>
 
 export type Reducer<State = any, Action = AnyAction> = reducers.Reducer<State, Action>
+
+export type BoundDispatch = store.BoundDispatch
+export type Dispatch<State = any> = store.Dispatch<State>
+export type Store<State = any> = store.Store<State>
+
+export type ThunkAction<ReturnType, State = any> = thunks.ThunkAction<ReturnType, State>
+export type BoundThunkDispatch<State = any> = thunks.BoundThunkDispatch<State>
+export type ThunkDispatch<State = any> = thunks.ThunkDispatch<State>
+export type ThunkfulStore<State = any> = thunks.ThunkfulStore<State>
 
 return {
 	Store = Store,
---
+++ src/types/thunks.lua
@@ -0,0 +1,23 @@
+local store = require(script.Parent.store)
+
+type IStore<State, Dispatch> = store.IStore<State, Dispatch>
+type IDispatch<Store> = store.IDispatch<Store>
+type BoundDispatch = store.BoundDispatch
+
+export type IThunkAction<ReturnType, Store> = (Store) -> ReturnType
+export type ThunkAction<ReturnType, State = any> = IThunkAction<ReturnType, ThunkfulStore<State>>
+
+export type IBoundThunkDispatch<Store> = <ReturnType>(
+	thunkAction: IThunkAction<ReturnType, Store>
+) -> ReturnType
+export type BoundThunkDispatch<State = any> = BoundDispatch & IBoundThunkDispatch<ThunkfulStore<State>>
+
+export type IThunkDispatch<Store> = <ReturnType>(
+	self: Store,
+	thunkAction: IThunkAction<ReturnType, Store>
+) -> ReturnType
+export type ThunkDispatch<State = any> = IDispatch<ThunkfulStore<State>> & IThunkDispatch<ThunkfulStore<State>>
+
+export type ThunkfulStore<State = any> = IStore<State, ThunkDispatch<State>>
+
+return nil
